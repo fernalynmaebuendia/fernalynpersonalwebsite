111aa8ff70c7ba7a5d377caa154d364efe7fa223
Fernalyn Mae Buendia <103086598+fernalynmaebuendia@users.noreply.github.com>
2023-05-04 16:15:34 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,40 +1,44 @@
-// To reload page and back to top of the page
-// window.addEventListener('beforeunload', function() {
-// 	window.scrollTo({
-// 		top:0,
-// 		behavior: "smooth"
-// 	})
-// });
+To reload page and back to top of the page
+window.addEventListener('beforeunload', function() {
+	window.scrollTo({
+	  top:0,
+		behavior: "smooth"
+	})
+});
 
 // Up button
 let mybutton = document.getElementById("arrow-up-button");
-
 window.onscroll = function() {scrollFunction()};
 // Display the "up" button when scrolling down
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-        mybutton.style.transition = '5s ease';
-    } 
-    else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    mybutton.style.transition = '5s ease';   
+  } 
+  else {
+    mybutton.style.display = "none";
+  }
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }	
 
-// Side menu
-var sidemenu = document.getElementById("side-menu");
-// Open menu
-function openMenu() {
-	sidemenu.style.right = "0px";
+// Close sidebar menu when click outside
+const sidebarIcon = document.querySelector(".sidebar-icon");
+const sidebar = document.querySelector(".sidebar");
+
+sidebarIcon.onclick = function() {
+  sidebarIcon.classList.toggle("active");
+  sidebar.classList.toggle("active_sidebar");
 }
-// Close menu
-function closeMenu() {
-	sidemenu.style.right = "-200px";
+
+document.onclick = function(e) {
+  if (!sidebarIcon.contains(e.target) && !sidebar.contains(e.target) ) {
+    sidebarIcon.classList.remove("active");
+    sidebar.classList.remove("active_sidebar");
+  }
 }
 
 // On scroll progress bar effects

8bee1769c2ad455252a0a8dc11526d1ce3bf285b
Fernalyn Mae Buendia <103086598+fernalynmaebuendia@users.noreply.github.com>
2023-05-04 17:47:40 +0800

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -6,68 +6,107 @@ window.addEventListener('beforeunload', function() {
 	})
 });
 
-// Up button
-let mybutton = document.getElementById("arrow-up-button");
-window.onscroll = function() {scrollFunction()};
-// Display the "up" button when scrolling down
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-    mybutton.style.transition = '5s ease';   
+// Scroll Up button functionality module
+function handleScrollUpButton() {
+  const upButton = document.getElementById("arrow-up-button");
+  // Display the "Up" button when scrolling down
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      upButton.style.display = "block";
+      upButton.style.transition = '5s ease';   
+    } 
+    else {
+      upButton.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document 
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  // Attach scroll event listener
+  window.addEventListener("scroll", scrollFunction);
+  // Attach click event listener
+  upButton.addEventListener("click", topFunction);
+}
+
+// Sidebar functionality module
+function handleSidebar() {
+  const sidebarIcon = document.querySelector(".sidebar-icon");
+  const sidebar = document.querySelector(".sidebar");
+  const sidebarLinks = document.querySelectorAll(".sidebar a");
+  
+  // Close sidebar menu when clicking on a sidebar link
+  sidebarLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      // Close the sidebar
+      sidebarIcon.classList.remove("active");
+      sidebar.classList.remove("active_sidebar");
+    });
+  });
+
+  function toggleSidebar() {
+    sidebarIcon.classList.toggle("active");
+    sidebar.classList.toggle("active_sidebar");
+  }
+  // Close sidebar menu when click outside
+  function closeSidebar(e) {
+    if (!sidebarIcon.contains(e.target) && !sidebar.contains(e.target)) {
+      sidebarIcon.classList.remove("active");
+      sidebar.classList.remove("active_sidebar");
+    }
+  }
+  // Attach click event listener to sidebar icon
+  sidebarIcon.addEventListener("click", toggleSidebar);
+  // Attach click event listener to document
+  document.addEventListener("click", closeSidebar);
+}
+
+// On scroll progress bar effects module
+function handleProgressBar() {
+  const skillContainer = document.querySelector(".header-text");
+  const percentageSpan = document.getElementsByClassName("percentage");
+  
+  function onScrollProgressBar() {
+    if (document.documentElement.scrollTop >= skillContainer.offsetTop) {
+      for (let i = 0; i < percentageSpan.length; i++) {
+        percentageSpan[i].style.width = percentageSpan[i].getAttribute("data-width");
+      }
+    } else {
+      for (let i = 0; i < percentageSpan.length; i++) {
+        percentageSpan[i].style.width = 0;
+      }
+    }
   } 
-  else {
-    mybutton.style.display = "none";
+  // Attach scroll event listener
+  window.addEventListener("scroll", onScrollProgressBar);
+}
+
+// Tab navigation click hover effects module
+function handleTabNavigation() {
+  const tablinks = document.getElementsByClassName("tab-links");
+  const tabcontents = document.getElementsByClassName("tab-contents");
+  
+  function opentab(tabname) {
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active-link");
+    }
+    for (let i = 0; i < tabcontents.length; i++) {
+      tabcontents[i].classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
   }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}	
-
-// Close sidebar menu when click outside
-const sidebarIcon = document.querySelector(".sidebar-icon");
-const sidebar = document.querySelector(".sidebar");
-
-sidebarIcon.onclick = function() {
-  sidebarIcon.classList.toggle("active");
-  sidebar.classList.toggle("active_sidebar");
-}
-
-document.onclick = function(e) {
-  if (!sidebarIcon.contains(e.target) && !sidebar.contains(e.target) ) {
-    sidebarIcon.classList.remove("active");
-    sidebar.classList.remove("active_sidebar");
-  }
-}
-
-// On scroll progress bar effects
-const skillContainer = document.querySelector(".header-text");
-const percentageSpan = document.querySelectorAll(".percentage");
-
-window.addEventListener('scroll', () => {
-  if (document.documentElement.scrollTop >= skillContainer.offsetTop) {
-    percentageSpan.forEach((percentage) => {
-      percentage.style.width = percentage.getAttribute("data-width");
-    });
-  } else {
-    percentageSpan.forEach((percentage) => {
-      percentage.style.width = 0;
+  // Attach click event listeners to tab links
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].addEventListener("click", function() {
+      opentab(this.dataset.tab);
     });
   }
-});
-
-// Tab navigation click hover effects
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname) {
-	for(tablink of tablinks) {
-		tablink.classList.remove("active-link");
-	}
-	for(tabcontent of tabcontents) {
-		tabcontent.classList.remove("active-tab");
-	}
-	event.currentTarget.classList.add("active-link");
-	document.getElementById(tabname).classList.add("active-tab")
 }
+
+// Call the modularized functions
+handleScrollUpButton();
+handleSidebar();
+handleProgressBar();
+handleTabNavigation();
